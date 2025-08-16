f8da5ef8c9116c770f3dcd70ca14ecc571f7aec4
tristinyun08 <tristinyun08@gmail.com>
2025-08-16 17:01:05 -0400

    create login page

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { authClient } from '$lib/auth-client';
+	import { goto } from '$app/navigation';
+
+	let email = '';
+	let password = '';
+</script>
+
+<h1>Sign in</h1>
+
+<form on:submit|preventDefault={() => authClient.signIn.email({ email, password }, {
+    onSuccess: () => goto('/')
+})}>
+	<label for="email">Email</label>
+	<input type="email" bind:value={email} id="email" />
+	<label for="password">Password</label>
+	<input type="password" bind:value={password} id="password" />
+	<button>Sign in</button>
+</form>
