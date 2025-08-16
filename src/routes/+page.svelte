a07f9f6fbfe01d1510fc621a53b64b23584e46ae
tristinyun08 <tristinyun08@gmail.com>
2025-08-15 21:28:06 -0400

    reactive variables

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,8 @@
+<script lang="ts">
+	let todos = $state([]);
+	let newTodoText = $state('');
+</script>
+
 <div class="font-serif min-h-screen bg-[#bba58f] flex items-center justify-center p-4">
 	<div class="bg-[#e8d9cd] p-5 rounded-lg shadow-lg w-full max-w-md">
         <h1 class="text-center text-[#333] text-3xl font-bold my-5">To-Do List</h1>
