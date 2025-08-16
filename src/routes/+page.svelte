1c47f60f1b0d45ff6a9adda778987c15bc40f34e
tristinyun08 <tristinyun08@gmail.com>
2025-08-15 21:20:01 -0400

    add app heading

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <div class="font-serif min-h-screen bg-[#bba58f] flex items-center justify-center p-4">
 	<div class="bg-[#e8d9cd] p-5 rounded-lg shadow-lg w-full max-w-md">
+        <h1 class="text-center text-[#333] text-3xl font-bold my-5">To-Do List</h1>
 	</div>
 </div>
