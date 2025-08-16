3f766ea9feb810beee6a6baa3f3cdabd90ae7d15
tristinyun08 <tristinyun08@gmail.com>
2025-08-16 17:00:36 -0400

    create register page

---
+++ src/routes/register/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { authClient } from '$lib/auth-client';
+	import { goto } from '$app/navigation';
+
+	let name = ''; 
+	let email = '';
+	let password = '';
+</script>
+
+<h1>Create an account</h1>
+
+<form on:submit|preventDefault={() => authClient.signUp.email({ name, email, password }, {
+    onSuccess: () => goto('/')
+})}>
+	<label for="name">Name</label>
+	<input type="text" bind:value={name} id="name" />
+
+	<label for="email">Email</label>
+	<input type="email" bind:value={email} id="email" />
+
+	<label for="password">Password</label>
+	<input type="password" bind:value={password} id="password" />
+	
+    <button>Continue</button>
+</form>
