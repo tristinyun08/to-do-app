b1335be2d60191e42ab0e7c66f882c9d432061e1
tristinyun08 <tristinyun08@gmail.com>
2025-08-16 16:55:14 -0400

    update scema

--- src/lib/server/auth.ts
+++ src/lib/server/auth.ts
@@ -1,6 +1,6 @@
 import { betterAuth } from 'better-auth';
 import { drizzleAdapter } from 'better-auth/adapters/drizzle';
-import { db } from '$lib/server/db';
+import { db } from './db/index.js';
 
 export const auth = betterAuth({
 	database: drizzleAdapter(db, {
--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,13 +1,49 @@
-import { pgTable, text, boolean, timestamp, jsonb } from 'drizzle-orm/pg-core';
-import type { Todo } from '../../types'; 
+import { pgTable, text, timestamp, boolean, jsonb } from 'drizzle-orm/pg-core';
+
+import type { Todo } from '../../types';
 
 export const user = pgTable('user', {
 	id: text('id').primaryKey(),
-	name: text('name'),
-	email: text('email').unique(),
-	emailVerified: boolean('emailVerified').default(false),
+	name: text('name'), 
+	email: text('email').notNull().unique(),
+	emailVerified: boolean('email_verified').default(false).notNull(),
 	image: text('image'),
-	createdAt: timestamp('createdAt', { mode: 'date' }).defaultNow(),
-	updatedAt: timestamp('updatedAt', { mode: 'date' }).defaultNow(),
+	createdAt: timestamp('created_at').defaultNow().notNull(),
+	updatedAt: timestamp('updated_at').defaultNow().notNull(),
 	todos: jsonb('todos').$type<Todo[]>().default([])
+});
+
+export const session = pgTable('session', {
+	id: text('id').primaryKey(),
+	expiresAt: timestamp('expires_at', {
+		withTimezone: true,
+		mode: 'date'
+	}).notNull(),
+	userId: text('user_id')
+		.notNull()
+		.references(() => user.id)
+});
+
+export const account = pgTable('account', {
+	userId: text('user_id')
+		.notNull()
+		.references(() => user.id),
+	providerId: text('provider_id').notNull(),
+	accountId: text('account_id').notNull(),
+	password: text('password'),
+	accessToken: text('access_token'),
+	refreshToken: text('refresh_token'),
+	accessTokenExpiresAt: timestamp('access_token_expires_at', {
+		withTimezone: true,
+		mode: 'date'
+	})
+});
+
+export const verification = pgTable('verification', {
+	id: text('id').primaryKey(),
+	identifier: text('identifier').notNull(),
+	value: text('value').notNull(),
+	expiresAt: timestamp('expires_at', { withTimezone: true, mode: 'date' }).notNull(),
+	createdAt: timestamp('created_at', { withTimezone: true, mode: 'date' }).defaultNow(),
+	updatedAt: timestamp('updated_at', { withTimezone: true, mode: 'date' }).defaultNow()
 });
